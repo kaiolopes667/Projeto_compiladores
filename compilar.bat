d99c216cdc024a3149b74c3ba5b72eb1b10afeba
christyanms <christyan228@gmail.com>
2025-07-07 16:58:50 -0300

    feat: adicionando comandos

--- compilar.bat
+++ compilar.bat
@@ -8,4 +8,13 @@ python saida2.py
 python main.py entrada3.txt saida3.py
 python saida3.py
 
+python main.py entrada4.txt saida4.py
+python saida4.py
+
+python main.py entrada5.txt saida5.py
+python saida5.py
+
+python main.py entrada6.txt saida6.py
+python saida6.py
+
 pause
